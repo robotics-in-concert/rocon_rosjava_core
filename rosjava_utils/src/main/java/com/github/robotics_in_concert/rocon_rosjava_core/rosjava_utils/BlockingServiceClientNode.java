373a1bf6882153d28df0baf41634ad445b75f797
dwlee <dwlee@yujinrobot.com>
2015-07-16 10:48:57 +0900

    update exception and merge from previous PR7

--- rosjava_utils/src/main/java/com/github/robotics_in_concert/rocon_rosjava_core/rosjava_utils/BlockingServiceClientNode.java
+++ rosjava_utils/src/main/java/com/github/robotics_in_concert/rocon_rosjava_core/rosjava_utils/BlockingServiceClientNode.java
@@ -5,6 +5,7 @@ package com.github.robotics_in_concert.rocon_rosjava_core.rosjava_utils;
 *****************************************************************************/
 
 import org.ros.exception.RemoteException;
+import org.ros.exception.RosRuntimeException;
 import org.ros.exception.ServiceNotFoundException;
 import org.ros.namespace.NameResolver;
 import org.ros.namespace.NodeNameResolver;
@@ -35,6 +36,9 @@ public class BlockingServiceClientNode<RequestType, ResponseType> {
         } catch (ServiceNotFoundException e) {
         	throw e;
         }
+		catch (RosRuntimeException e) {
+			throw e;
+		}
 		srvClient.call(request, this.setupListener());
 	}
 
